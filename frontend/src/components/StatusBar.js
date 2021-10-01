fda1000c716f9703be0f9f43c111f352f5e7a9c2
Alen Rubilar <lclc.alen@gmail.com>
2021-10-01 15:51:48 -0300

    Add (constant) loading message in StatusBar
    TODO: implement StatusBar controller

--- frontend/src/components/StatusBar.js
+++ frontend/src/components/StatusBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/StatusBar.css';
+import {Spinner, SpinnerSize, Text} from '@fluentui/react';
 
 function StatusBar() {
     return (
-        <div className="statusBar">
-            Status Bar
+        <div className="statusBar flex items-center ml-2 space-x-2">
+            <Spinner 
+                size={SpinnerSize.xSmall} />
+            <Text variant="small">Loading</Text> 
         </div>
     )
 }

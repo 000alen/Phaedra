425267d30acc6c50ff2c01a447f5b4b388bfe6c9
Alen Rubilar <lclc.alen@gmail.com>
2021-10-02 17:39:33 -0300

    Implement Cell as a stateful component

--- frontend/src/components/Notebook/Cell.js
+++ frontend/src/components/Notebook/Cell.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown';
 import { theme } from '../../index';
 import { v4 as uuidv4 } from 'uuid';
@@ -16,59 +16,82 @@ export function createCell(id, data, content) {
     }
 }
 
-function Cell({ id, pageController, notebookController, data, content, pageId, active, editing }) {
-    const [contentText, setContentText] = React.useState(content);
 
-    const handleSelection = (event) => {
-        if (active && editing) return;
-        notebookController.handleSelection(pageId, id);
-        event.stopPropagation();
-    };
+class Cell extends Component {
+    constructor(props) {
+        super(props);
 
-    const handleChange = (event) => {
-        setContentText(event.target.value);
+        const {id, pageId, pageController, notebookController } = props;
+        const {content} = props;
+
+        this.state = {
+            id: id,
+            pageId: pageId,
+            pageController: pageController,
+            notebookController: notebookController,
+            content: content
+        }
     }
 
-    const handleSet = () => {
-        notebookController.setCellContent(pageId, id, contentText, true);
-    }
+    render() {
+        const {id, pageId, pageController, notebookController } = this.state;
+        const { data, content, active, editing } = this.props;
 
-    const style = { 
-        backgroundColor: theme.palette.neutralLight,
-        border: `1px solid ${theme.palette.neutralLight}`
-    }
-    const activeStyle = {
-        backgroundColor: theme.palette.neutralLight,
-        border: `1px solid ${theme.palette.themePrimary}`
-    }
+        const handleSelection = (event) => {
+            if (active && editing) return;
+            notebookController.handleSelection(pageId, id);
+            event.stopPropagation();
+        };
 
-    const wrapperClass = mergeStyles({
-        selectors: {
-          '& > .ms-Shimmer-container': {
-            margin: '10px 0',
-          },
-        },
-    });
+        const handleChange = (event) => {
+            this.setState((state) => {
+                return {
+                    ...state,
+                    content: event.target.value
+            }});
+        }
 
-    return (
-        (active && editing) ? <div className="cell m-2 space-y-2">
-            <TextField 
-                value={contentText} 
-                onChange={handleChange}
-                multiline 
-                autoAdjustHeight />
-            <PrimaryButton 
-                text="Set"
-                onClick={handleSet} /> 
-        </div> : <div 
-            className={`cell p-2 m-2 rounded-sm shadow-md text-justify ${wrapperClass}`} 
-            style={active ? activeStyle : style} 
-            onClick={handleSelection}>
-                <ReactMarkdown
-                    children={content}
-                    linkTarget="_blank" />
-        </div>
-    );
+        const handleSet = () => {
+            notebookController.setCellContent(pageId, id, this.state.content, true);
+        }
+
+        const style = { 
+            backgroundColor: theme.palette.neutralLight,
+            border: `1px solid ${theme.palette.neutralLight}`
+        }
+        const activeStyle = {
+            backgroundColor: theme.palette.neutralLight,
+            border: `1px solid ${theme.palette.themePrimary}`
+        }
+
+        const wrapperClass = mergeStyles({
+            selectors: {
+            '& > .ms-Shimmer-container': {
+                margin: '10px 0',
+            },
+            },
+        });
+
+        return (
+            (active && editing) ? <div className="cell m-2 space-y-2">
+                <TextField 
+                    value={this.state.content} 
+                    onChange={handleChange}
+                    multiline 
+                    autoAdjustHeight />
+                <PrimaryButton 
+                    text="Set"
+                    onClick={handleSet} /> 
+            </div> : <div 
+                className={`cell p-2 m-2 rounded-sm shadow-md text-justify ${wrapperClass}`} 
+                style={active ? activeStyle : style} 
+                onClick={handleSelection}>
+                    <ReactMarkdown
+                        children={content}
+                        linkTarget="_blank" />
+            </div>
+        );
+    }
 }
 
 export default Cell;

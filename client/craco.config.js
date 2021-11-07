afc24243d1656baf9c32c38633fcbc3c00063b8e
Alen Rubilar <lclc.alen@gmail.com>
2021-11-07 18:10:44 -0300

    Fix Tailwind

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js, jsx, ts, tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };

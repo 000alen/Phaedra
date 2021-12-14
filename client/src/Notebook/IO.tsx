e94a084b01fa9443f5c6e11aa6d2490a4746aba5
Alen Rubilar <lclc.alen@gmail.com>
2021-12-14 15:25:14 -0300

    Remove Python backend

--- client/src/Notebook/IO.tsx
+++ client/src/Notebook/IO.tsx
@@ -1,5 +1,4 @@
 import {
-  base64,
   OpenDialogOptions,
   readFileSync,
   showOpenDialog,
--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -1,5 +1,1 @@
-declare module "gpt-3-encoder" {
-  export function encode(text: string): number[];
-
-  export function decode(tokens: number[]): string;
-}
+declare module "pdfjs-dist/legacy/build/pdf.worker.entry";
